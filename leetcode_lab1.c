3c70f63a18031e5536b1c2040c63acb965144717
HaaidQazi <117591068+HaaidQazi@users.noreply.github.com>
2023-06-17 11:44:32 +0530

    Create leetcode_lab1.c

---
+++ leetcode_lab1.c
@@ -0,0 +1,24 @@
+typedef struct {
+    
+} MyHashSet;
+
+
+MyHashSet* myHashSetCreate() {
+    
+}
+
+void myHashSetAdd(MyHashSet* obj, int key) {
+  
+}
+
+void myHashSetRemove(MyHashSet* obj, int key) {
+  
+}
+
+bool myHashSetContains(MyHashSet* obj, int key) {
+  
+}
+
+void myHashSetFree(MyHashSet* obj) {
+    
+}
