cd820240c8d34a6a621aad7d97be63b4c7656855
HaaidQazi <117591068+HaaidQazi@users.noreply.github.com>
2023-08-20 16:51:46 +0530

    Update BFS.c

--- BFS.c
+++ BFS.c
@@ -1,35 +1,44 @@
-int graph[20][20];
-void DFS(int i,int vis[],int n)
-{
-    int j;
-    printf("%d ",i);
-    vis[i]=1;
-    for(j=0;j<n;j++)
-    {
-        if(graph[i][j]==1 && vis[j]==0)
-        {
-            DFS(j,vis,n);
-        }
-    }
+  #include<stdio.h>
+int a[20][20], q[20], visited[20], n, i, j, f = 0, r = -1;
+
+void bfs(int v) {
+ for(i = 1; i <= n; i++)
+ if(a[v][i] && !visited[i])
+ q[++r] = i;
+ if(f <= r) {
+ visited[q[f]] = 1;
+ bfs(q[f++]);
+ }
 }
-void main()
-{
-    int n,i,j,top=-1;
-    printf("Enter the number of vertices:\n");
-    scanf("%d",&n);
-    printf("Enter the adjacency matrix representing the graph:\n");
-    
-    int vis[n],st[n];
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            scanf("%d",&graph[i][j]);
-        }
-    }
-    for(int i=0;i<n;i++)
-    {
-        vis[i]=0;
-    }
-    DFS(0,vis,n);
+
+void main() {
+ int v;
+ printf("\n Enter the number of vertices:");
+ scanf("%d", &n);
+ 
+ for(i=1; i <= n; i++) {
+ q[i] = 0;
+ visited[i] = 0;
+ }
+ 
+ printf("\n Enter graph data in matrix form:\n");
+ for(i=1; i<=n; i++) {
+ for(j=1;j<=n;j++) {
+ scanf("%d", &a[i][j]);
+ }
+ }
+ 
+ printf("\n Enter the starting vertex:");
+ scanf("%d", &v);
+ bfs(v);
+ printf("\n The node which are reachable are:\n");
+ 
+ for(i=1; i <= n; i++) {
+ if(visited[i])
+ printf("%d\t", i);
+ else {
+ printf("\n Bfs is not possible. Not all nodes are reachable");
+ break;
+ }
+ }
 }
